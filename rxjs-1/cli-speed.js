76757111fe141e685269c73cb1b5e8ee14f51048
MatthD <matthias.dieudonne@gmail.com>
2019-06-30 11:36:45 +0200

    Fix calculation, using difference between prev & current data

--- rxjs-1/cli-speed.js
+++ rxjs-1/cli-speed.js
@@ -5,7 +5,7 @@
  */
 const kuler = require('kuler');
 const { fromEvent, merge } = require('rxjs');
-const { map, filter, takeUntil, throttleTime } = require('rxjs/operators');
+const { map, filter, takeUntil, throttleTime, pairwise } = require('rxjs/operators');
 
 /**
  * Internal dependencies
@@ -20,7 +20,7 @@ const speed$ = getCarSpeed(race, carName);
 let subs = speed$.subscribe({
   next: (speed) => {
     if (isNaN(speed)) return; // speed is not a number, stop here
-    process.stdout.write(`\rSpeed: ${speed.toFixed(1)} km/h`)
+    process.stdout.write(`Speed: ${speed.toFixed(1)} km/h\r`)
   },
   error: (err) => {
     console.error(kuler(`An error Happen reading car speed: ${err}`, 'red'))
@@ -41,16 +41,24 @@ race.start();
 function getCarSpeed(race, reqCarName) {
   let endInfo$ = fromEvent(race, 'end');
   return fromEvent(race, 'data').pipe(
+    throttleTime(200),
     // Keep Only requested car by name
     filter(({ carName }) => carName === reqCarName),
     // Filter bad value when time=0 preventing NaN
     filter(({ time }) => time != 0),
-    throttleTime(300),
-    // calculate speed
-    map(function ({ time, xLocation }) { // time in ms, xlocation in m
-      let xlocationKm = xLocation / 1000;
-      let timeHour = time / (1000 * 3600);
-      return (xlocationKm / timeHour)
+    // get the previous value with the current in an array
+    pairwise(),
+    // calculate mean speed
+    map(function ([prev, current]) { // time in ms, xlocation in m
+      let { time: prevTime, xLocation: prevXLocation } = prev;
+      let { time, xLocation } = current;
+
+      // Calculate the mean between previous & current data  
+      let duration = time - prevTime;
+      let distance = xLocation - prevXLocation;
+      let distanceKm = distance / 1000;
+      let durationHour = duration / (1000 * 3600);
+      return (distanceKm / durationHour)
     }),
     // When race emit 'end' we should complete subscriber
     takeUntil(endInfo$)
